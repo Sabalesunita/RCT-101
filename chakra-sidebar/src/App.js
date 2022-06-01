4c4ba011b519b49dfccdbe5fee0f201befd7fb98
Sabalesunita <97454504+Sabalesunita@users.noreply.github.com>
2022-06-01 14:06:49 +0530

    chakra-sidebar

---
+++ chakra-sidebar/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleSidebar from './components/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+     <SimpleSidebar/>
+    </div>
+  );
+}
+
+export default App;
